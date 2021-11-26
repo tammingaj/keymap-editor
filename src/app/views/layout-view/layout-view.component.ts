76e67a4be05152a667b2fc831fe74109470de376
tammingaj <janpeter@tammingas.nl>
2021-11-26 18:15:37 +0100

    highlight keys on same row or column

--- src/app/views/layout-view/layout-view.component.ts
+++ src/app/views/layout-view/layout-view.component.ts
@@ -11,16 +11,27 @@ import {Subscription} from "rxjs";
 export class LayoutViewComponent implements OnInit {
 
   private keysSubscription: Subscription = new Subscription();
+  private currentKeySubscription: Subscription = new Subscription();
 
-  public keys: Array<KeyConfig> = new Array<KeyConfig>()
+  public keys: Array<KeyConfig> = new Array<KeyConfig>();
+  public currentKey: KeyConfig = KeyConfig.getInstance();
+  public highlightRow: boolean = false;
+  public highlightColumn: boolean = false;
 
   constructor(public keyMapService: KeyMapService) { }
 
   ngOnInit(): void {
+    this.currentKey.row = -1;
+    this.currentKey.column = -1;
     console.log('subscribing to keys');
     this.keysSubscription = this.keyMapService.keys$.subscribe(
       (keys) => {
         this.keys = keys;
+      }
+    );
+    this.currentKeySubscription = this.keyMapService.currentKey$.subscribe(
+      (key) => {
+        this.currentKey = key;
       }
     );
   }
@@ -29,6 +40,9 @@ export class LayoutViewComponent implements OnInit {
     console.log('unsubscribing from keys');
     if(this.keysSubscription) {
       this.keysSubscription.unsubscribe();
+    }
+    if(this.currentKeySubscription) {
+      this.currentKeySubscription.unsubscribe();
     }
   }
 
@@ -45,8 +59,27 @@ export class LayoutViewComponent implements OnInit {
       'transform-box': 'fill-box',
       'transform-origin': 'center',
       'transform': 'rotate(' + key.angle + 'deg)',
-      'rx': '4px'
+      'rx': '4px',
+      'fill': this.getFill(key),
     }
+  }
+
+  private getFill(key:KeyConfig): string {
+    let color: string = '#00';
+    if (key.row === this.currentKey.row && key.column === this.currentKey.column) {
+      color = '#33'
+    }
+    if (key.row === this.currentKey.row && this.highlightRow) {
+      color = color + '33'
+    } else {
+      color = color + '00'
+    }
+    if (key.column === this.currentKey.column && this.highlightColumn) {
+      color = color + '33'
+    } else {
+      color = color + '00'
+    }
+    return color;
   }
 
   selectKey(key:KeyConfig) {
