349012a81963947022d824a56c303b3afaeec369
tammingaj <janpeter@tammingas.nl>
2022-01-28 14:14:47 +0100

    Validation on keymap name input field

--- src/app/components/new-keymap-modal/new-keymap-modal.component.ts
+++ src/app/components/new-keymap-modal/new-keymap-modal.component.ts
@@ -35,4 +35,14 @@ export class NewKeymapModalComponent implements OnInit {
     this.activeModal.dismiss('cancel click');
   }
 
+  keyPressAlphanumeric(event: any) {
+    let inp = String.fromCharCode(event.keyCode);
+    if (/[a-zA-Z0-9-_]/.test(inp)) {
+      return true;
+    } else {
+      event.preventDefault();
+      return false;
+    }
+  }
+
 }
